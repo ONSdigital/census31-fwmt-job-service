dfa95dfe1b06a9c1b65fccbd7bf764a1507e8a87
sdiaz-ons <simon.diaz@ext.ons.gov.uk>
2026-07-14 16:31:28 +0100

    Adjust application config for runtime flag refresh

--- src/main/java/uk/gov/ons/census/fwmt/jobservice/Application.java
+++ src/main/java/uk/gov/ons/census/fwmt/jobservice/Application.java
@@ -2,19 +2,20 @@ package uk.gov.ons.census.fwmt.jobservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+    "uk.gov.ons.census.fwmt.jobservice",
+    "uk.gov.ons.census.fwmt.events",
+    "uk.gov.census.ffa.storage.utils"
+})
 @EnableIntegration
 @EnableRetry
 @EnableScheduling
 @EnableJpaRepositories("uk.gov.ons.census.fwmt.jobservice.repository")
-@ComponentScan({"uk.gov.ons.census.fwmt.jobservice", "uk.gov.ons.census.fwmt.events",
-    "uk.gov.census.ffa.storage.utils"})
 public class Application {
 
   public static final String APPLICATION_NAME = "FWMT Gateway Job Service";
